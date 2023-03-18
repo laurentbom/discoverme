932af1da186f0444abfd769ac06f4df42bf5775e
LaurentB <laurent.bomal@gmail.com>
2023-03-18 10:46:52 +0100

    18mars

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 // Variables
-var burgerMenu = document.querySelector(".burger-menu");
-var nav = document.querySelector(".header__nav");
+const burgerMenu = document.querySelector(".burger-menu");
+const nav = document.querySelector(".header__nav");
 
-var home = document.querySelector("body");
-var aboutme = document.querySelector(".aboutme");
-var portfolio = document.querySelector(".portfolio");
-var contact = document.querySelector(".contact");
+const home = document.querySelector("body");
+const aboutme = document.querySelector(".aboutme");
+const portfolio = document.querySelector(".portfolio");
+const contact = document.querySelector(".contact");
 
-var btnHeader = document.querySelector(".btn__head")
-var btnHome = document.querySelector(".btn__home");
-var btnPortfolio = document.querySelector(".btn__portfolio");
-var btnContact = document.querySelector(".btn__contact");
+const btnHeader = document.querySelector(".btn__head")
+const btnHome = document.querySelector(".btn__home");
+const btnPortfolio = document.querySelector(".btn__portfolio");
+const btnContact = document.querySelector(".btn__contact");
 
 // Nav disparait au scroll
-var lastScroll = 0;
-var headerNav = document.querySelector(".nav-container");
-var header = document.querySelector(".header");
+let lastScroll = 0;
+const headerNav = document.querySelector(".nav-container");
+const header = document.querySelector(".header");
 
 
 window.addEventListener("scroll", function(){
-  var scrolled = window.scrollY;
-  var hauteurNav = header.offsetHeight - 50;
+  let scrolled = window.scrollY;
+  let hauteurNav = header.offsetHeight - 50;
   if(scrolled > hauteurNav){
     headerNav.classList.add('white');
   } else{
@@ -64,19 +64,19 @@ burgerMenu.addEventListener("click", function(){
   nav.classList.toggle("open");
 })
 
-var cursor = document.querySelector('.cursor');
-var cursorinner = document.querySelector('.cursor2');
-var a = document.querySelectorAll('a');
+const cursor = document.querySelector('.cursor');
+const cursorinner = document.querySelector('.cursor2');
+let a = document.querySelectorAll('a');
 
 document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
+  let x = e.clientX;
+  let y = e.clientY;
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
 });
 
 document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
+  let x = e.clientX;
+  let y = e.clientY;
   cursorinner.style.left = x + 'px';
   cursorinner.style.top = y + 'px';
 });
